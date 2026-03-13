05435787048ca196da6f2368731070d749229afc
KeerthiSri2222 <keerthisrinunna@gmail.com>
2026-03-13 13:53:46 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.log(err);
   }
